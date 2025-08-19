252f62bb0c2599e52ba67c2a5f676f15dea994c7
Kourosh Alasti <coding@kouroshalasti.com>
2025-08-18 17:08:34 -0700

    fix: remove posthog provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./utils/router";
-import { PostHogProvider } from "posthog-js/react";
 
 import "./index.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
-const options = {
-    api_host: import.meta.env.VITE_POSTHOG_HOST!,
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <PostHogProvider
-            apiKey={import.meta.env.VITE_POSTHOG_KEY!}
-            options={options}
-        >
-            <RouterProvider router={router} />
-        </PostHogProvider>
+        <RouterProvider router={router} />
     </React.StrictMode>,
 );
