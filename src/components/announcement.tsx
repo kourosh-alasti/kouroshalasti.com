30d7917015aa1184c286bcbd72f3cfe44f8f849f
Kourosh Alasti <coding@kouroshalasti.com>
2025-11-05 20:59:53 -0800

    ref: migrate to phosphor icons

--- src/components/announcement.tsx
+++ src/components/announcement.tsx
@@ -1,14 +1,15 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
-
+import { WarningCircleIcon } from "@phosphor-icons/react";
 
 const AnnouncementSection = () => {
     return (
-        <div className="rounded-sm border border-slate-500 p-4 shadow-sm w-full flex flex-row items-center gap-2">
-            <FontAwesomeIcon icon={faCircleExclamation} className="size-6 "/>
-            <h1>FutDrafts won 3rd Place at CSU Fullerton's first ever Computer Science Showcase. </h1>
+        <div className="flex w-full flex-row items-center gap-2 rounded-sm border border-slate-500 p-4 shadow-sm">
+            <WarningCircleIcon className="size-6" />
+            <h1>
+                FutDrafts won 3rd Place at CSU Fullerton's first ever Computer
+                Science Showcase.
+            </h1>
         </div>
-    )
-}
+    );
+};
 
 export default AnnouncementSection;
