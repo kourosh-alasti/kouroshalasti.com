626f9cda01a111fd537b66d2cba2a40b0f43c3f7
Kourosh Alasti <coding@kouroshalasti.com>
2025-11-05 21:29:21 -0800

    Refactor ODS to use NPM package (#20)
    * build: install ods package
    
    * ref: remove ods script
    
    * feat: init ods in root
    
    * ref: migrate to ods npm package

--- src/pages/layouts/root.tsx
+++ src/pages/layouts/root.tsx
@@ -1,14 +1,25 @@
+import { configure } from "onedollarstats";
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 function RootLayout() {
-  return (
-    <div className="mx-auto flex h-full min-h-full w-full max-w-(--breakpoint-2xl) flex-col items-center justify-center">
-      <Outlet />
-      <footer className="gap-1 overflow-hidden">
-        &copy; 2026 Kourosh Alasti
-      </footer>
-    </div>
-  );
+    useEffect(() => {
+        configure({
+            trackLocalhostAs: "kouroshalasti.com",
+            collectorUrl: "https://collector.onedollarstats.com/events",
+            autocollect: true,
+            hashRouting: false,
+        });
+    });
+
+    return (
+        <div className="mx-auto flex h-full min-h-full w-full max-w-(--breakpoint-2xl) flex-col items-center justify-center">
+            <Outlet />
+            <footer className="gap-1 overflow-hidden">
+                &copy; 2026 Kourosh Alasti
+            </footer>
+        </div>
+    );
 }
 
 export default RootLayout;
